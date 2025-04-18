ff6395b8db37d88e6131f835a2fc7cb5644d7a27
jhnstn <jhnstn@pm.me>
2025-04-18 14:09:02 -0400

    improve loging

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const chalk = require("chalk");
 
 const app = express();
 const port = process.argv[2] || 3000;
 
+// CORS middleware to allow cross-origin requests from the specified origin
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+
+    // Handle OPTIONS requests
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 // Middleware to log requests
 app.use((req, res, next) => {
-    console.log(`Received ${req.method} request to ${req.url}`);
+    console.log(
+        chalk.blue.bold(`Received ${chalk.green(req.method)} request to ${chalk.yellow(req.url)}`)
+    );
     next();
 });
 
@@ -17,7 +33,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Log request body
 app.use((req, res, next) => {
     if (Object.keys(req.body).length > 0) {
-        console.log("Request Body:", req.body);
+        console.log(chalk.magenta.bold("Request Body:"));
+        console.log(chalk.cyan(JSON.stringify(req.body, null, 2)));
     }
     next();
 });
@@ -28,5 +45,5 @@ app.all("*", (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(chalk.green.bold(`Server running on ${chalk.underline(`http://localhost:${port}`)}`));
 });
